01a54b3e6cdc4fc5b30a7cec0a5d6b1e22c58ebe
moustafa2121 <moustafa2121@gmail.com>
2024-02-07 16:40:45 +0000

    deployment works

--- ProjectFlashback_b/settings.py
+++ ProjectFlashback_b/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '217698d9-5281-4a08-b840-906e61de2d77'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
